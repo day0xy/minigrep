6f1b50aef0438e66aefad35e33a6304b02b249c4
day0xy <jasonfan1337@gmail.com>
2024-07-14 23:12:44 +0800

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::error::Error;
-use std::{env, fs, process};
+use std::{env, process};
 
+use minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -12,31 +12,8 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    run(config);
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-//struct Config method
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments!");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error:{e}");
+        process::exit(1);
     }
-}
-
-//增加错误处理
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-
-    println!("With text:\n{contents}");
-    Ok(())
 }
