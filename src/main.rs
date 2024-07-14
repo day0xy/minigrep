89e6922c829de99eb609e01b621f7def034c3ab8
day0xy <jasonfan1337@gmail.com>
2024-07-14 22:41:47 +0800

    extract  function

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 // in main.rs
+use std::error::Error;
 use std::{env, fs, process};
 
 fn main() {
@@ -15,40 +16,13 @@ fn main() {
     run(config);
 }
 
-fn run(config: Config) {
-    let contents =
-        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
-
-    println!("With text:\n{contents}");
-}
-
 struct Config {
     query: String,
     file_path: String,
 }
 
-// fn parse_config(args: &[String]) -> Config {
-//     //解析命令行参数
-//     let query = args[1].clone();
-//     //clone函数避免所有权的转移
-//     let file_path = args[2].clone();
-
-//     // 返回值表达式
-//     Config { query, file_path }
-// }
-
 //struct Config method
 impl Config {
-    // fn new(args: &[String]) -> Config {
-    //     if args.len()<3{
-    //         panic!("not enough arguments!");
-    //     }
-    //     let query = args[1].clone();
-    //     let file_path = args[2].clone();
-
-    //     Config { query, file_path }
-    // }
-
     fn build(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
             return Err("not enough arguments!");
@@ -58,4 +32,11 @@ impl Config {
 
         Ok(Config { query, file_path })
     }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    println!("With text:\n{contents}");
+    Ok(())
 }
