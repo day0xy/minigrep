180cd98578dc963599b245015e31335094f649f3
day0xy <jasonfan1337@gmail.com>
2024-07-15 10:19:54 +0800

    1

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::{env, process};
 use minigrep::Config;
 fn main() {
 
-    //直接传入args迭代器
+    //传入args迭代器
     let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
